d42498d2423da260cd688ca6ac456ec08b1276d7
Julian Gaal <juliangaal@users.noreply.github.com>
2019-05-24 19:35:18 +0200

    disable gstreamer

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ sudo make install
 cd $HOME/install_stuff
 sudo apt-get install -y gcc g++ gtk2.0 ffmpeg python3-numpy python-numpy
 # gstreamer
-sudo apt-get install -y libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+#sudo apt-get install -y gstreamer1.0*
 
 if [ ! -d "opencv" ]; then
   git clone https://github.com/opencv/opencv.git
