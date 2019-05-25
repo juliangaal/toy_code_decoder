399e62e727483b9b81ae930bf5269ac6eb94835a
juliangaal <juliangaal@protonmail.com>
2019-05-25 11:58:59 +0200

    basic script for testing rotation output

---
+++ show.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import math
+
+plt.plot([92.5,128.5, 162.5, 198.5], [90.5, 90.5, 90.5, 90.5], color='b')
+plt.plot([92.5, 128.5, 162.5, 198.5], [130.5, 130.5, 130.5, 130.5], color='r')
+plt.show()
