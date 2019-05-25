45438015e7932b6235ca8299412491ae9384f4b8
juliangaal <juliangaal@protonmail.com>
2019-05-25 18:20:45 +0200

    toy decoder library

---
+++ src/toy_decoder.cpp
@@ -0,0 +1,159 @@
+//
+// Created by julian on 5/25/19.
+//
+
+#include <toy_decoder/toy_decoder.hpp>
+#include <fmt/format.h>
+#include <exception>
+
+using namespace toy_decoder::util;
+using namespace toy_decoder;
+
+ToyDecoder::ToyDecoder(cv::Mat &img) : img{img}, params{}, keypoints{}, orientation_line{} {
+    // set simple blob detector params
+    // Change thresholds
+    this->params.minThreshold = 10;
+    this->params.maxThreshold = 200;
+
+    // Filter by Area.
+    this->params.filterByArea = true;
+    this->params.minArea = 100;
+    this->params.maxArea = 1000000;
+
+    // Filter by Circularity
+    this->params.filterByCircularity = true;
+    this->params.minCircularity = 0.01;
+
+    keypoints.reserve(10);
+    orientation_line.reserve(2);
+}
+
+ToyDecoder::ToyDecoder(cv::Mat &img, cv::SimpleBlobDetector::Params params) : img{img}, params{params}, keypoints{},
+                                                                              orientation_line{} {
+    keypoints.reserve(10);
+    orientation_line.reserve(2);
+}
+
+void ToyDecoder::calculate_keypoints(Mark_Keypoints mark) {
+#if CV_MAJOR_VERSION < 3   // If you are using OpenCV 2
+    // Set up detector with params and detect blobs
+    cv::SimpleBlobDetector detector(this->params);
+    detector.detect(this->img, this->keypoints);
+#else
+    // Set up detector with params and detect
+    cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+    detector->detect(this->img, this->keypoints);
+#endif
+
+    switch (mark) {
+        case YES:
+            cv::drawKeypoints(this->img, keypoints, this->img, cv::Scalar(0, 0, 255),
+                              cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+            break;
+        case NO:
+        default:
+            break;
+    }
+}
+
+std::tuple<float, float, bool> ToyDecoder::calculate_rotation() {
+    if (this->keypoints.size() != 10) {
+        fmt::print("Not enough keypoint found!\n");
+        return {0.0, 0.0, false};
+    }
+
+    // partition in bit points and orientation points
+    auto bit_end_it = std::stable_partition(keypoints.begin(), keypoints.end(), [&](const auto &p) {
+        return !color::is_black(this->img, p) and !color::is_red(this->img, p);
+    });
+
+    if (std::distance(bit_end_it, keypoints.end()) != 2) {
+        fmt::print("Couldn't find line. Line length {}", std::distance(bit_end_it, keypoints.end()));
+        return {0.0, 0.0, false};
+    }
+
+    // move orientations points to new vec and delete from old
+    this->orientation_line = std::vector<cv::KeyPoint>(std::make_move_iterator(bit_end_it),
+                                                       std::make_move_iterator(keypoints.end()));
+    keypoints.erase(bit_end_it, keypoints.end());
+
+    // partition again: red point is now in front: position 0
+    std::stable_partition(this->orientation_line.begin(), this->orientation_line.end(),
+                          [&](const auto &p) { return color::is_red(this->img, p); }); // ignore iterator
+    geo::to_cartesian(this->orientation_line[0].pt);
+    geo::to_cartesian(this->orientation_line[1].pt);
+    const auto &red_point = this->orientation_line[0].pt;
+    const auto &black_point = this->orientation_line[1].pt;
+
+    // calculate vector that is spanned by red and black point
+    auto vec = geo::connecting_vector(red_point, black_point);
+    // calculate absolute orientation
+    float orientation = std::atan(vec.y / vec.x) * 180.0 / M_PI;
+    // right hand rule: turn to left is negative, turn to right is positive
+    orientation *= -1;
+    float img_rotation = orientation;
+
+    // It's not enough to just turn parallel (enough for orientation estimation),
+    // but the red dot has to be to the right of the black one
+    // therefore a manual flip is necessary, if following condition is met
+    // saved in img_rotation
+    if (red_point.x < black_point.x) {
+        orientation < 0 ? img_rotation -= 180.0 : img_rotation += 180;
+    }
+
+    return {orientation, img_rotation, true};
+}
+
+void ToyDecoder::rotate_img(toy_decoder::util::units::Degrees degrees) {
+    auto shape = cv::Point2f{static_cast<float>(this->img.cols / 2.), static_cast<float>(this->img.rows / 2.)};
+    auto img_rot_matrix = cv::getRotationMatrix2D(shape, degrees.to_deg(), 1);
+    cv::warpAffine(this->img, this->img, img_rot_matrix, cv::Size{this->img.cols, this->img.rows});
+}
+
+void ToyDecoder::rotate_keypoints(toy_decoder::util::units::Degrees degrees) {
+    // apply rotation to keypoints
+    for (auto &keypoint: keypoints) {
+        geo::to_cartesian(keypoint.pt);
+        calc::rotate(keypoint.pt, degrees);
+    }
+
+    // apply rotation to this->orientation_line
+    for (auto &keypoint: this->orientation_line) {
+        // no need to convert to cartesian, already done in calculate_rotation
+        calc::rotate(keypoint.pt, degrees);
+    }
+}
+
+std::tuple<int, bool> ToyDecoder::decode() {
+    if ((this->orientation_line.size() + this->keypoints.size()) != 10) {
+        fmt::print("Can't decode less than 10 points");
+        return {0, false};
+    }
+
+    // TODO
+    // [x] separate with std::partition in bits to decode
+    // [ ] catch erroneuos rotations
+    // [ ] decode bits: color/size?!
+    const auto separator_it = std::partition(this->keypoints.begin(), this->keypoints.end(), [&](const auto &p) {
+        // line is rotated => any points below/above points below to either bits
+        return p.pt.y > this->orientation_line[0].pt.y;
+    });
+
+    // Sort keypoints for bit reading: first half of vector will be keypoints above
+    std::sort(this->keypoints.begin(), separator_it, [](const auto &a, const auto &b) { return a.pt.x < b.pt.x; });
+    std::sort(separator_it, this->keypoints.end(), [](const auto &a, const auto &b) { return a.pt.x < b.pt.x; });
+
+    // TODO decoding
+
+    return {1, true};
+}
+
+void ToyDecoder::save_img(std::string name) {
+    cv::imwrite(name, this->img);
+}
+
+void ToyDecoder::open_img() {
+    cv::imshow("rotated image", this->img);
+    cv::waitKey(0);
+}
+
--- src/util.cpp
+++ src/util.cpp
@@ -2,16 +2,9 @@
 // Created by julian on 5/24/19.
 //
 
-#ifndef HWCPP_UTIL_HPP
-#define HWCPP_UTIL_HPP
+#include <toy_decoder/util.hpp>
 
-#include <cmath>
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include <vector>
-
-namespace hw {
-namespace util {
+using namespace toy_decoder::util;
 
 size_t decode(std::vector<cv::KeyPoint>::const_iterator begin,
               std::vector<cv::KeyPoint>::const_iterator end) {
@@ -23,41 +16,7 @@ size_t decode(std::vector<cv::KeyPoint>::const_iterator begin,
     return val;
 }
 
-namespace units {
-
-class Degrees {
-public:
-    explicit constexpr Degrees(float amount) : amount(amount) {}
-
-    constexpr Degrees() = delete;
-
-    ~Degrees() = default;
-
-    constexpr float to_rad() {
-        return this->amount * M_PI / 180.0;
-    }
-
-private:
-    float amount;
-};
-
-} // namespace hw::util::units
-
-namespace color {
-
-enum Color {
-    RED,
-    GREEN,
-    BLUE,
-    BLACK,
-};
-
-const static cv::Vec3b RGBRED = cv::Vec3b{255, 0, 0};
-const static cv::Vec3b RGBGREEN = cv::Vec3b{0, 255, 0};
-const static cv::Vec3b RGBBLUE = cv::Vec3b{0, 0, 255};
-const static cv::Vec3b RGBBLACK = cv::Vec3b{0, 0, 0};
-
-bool same_color(const cv::Mat &im, const cv::KeyPoint &point, Color color, float tolerance = 40.0) {
+bool color::same_color(const cv::Mat &im, const cv::KeyPoint &point, Color color, float tolerance) {
     // opencv idiots thought it was a good idea to return color in BGR
     auto true_color = im.at<cv::Vec3b>(point.pt);
     switch (color) {
@@ -86,21 +45,15 @@ bool same_color(const cv::Mat &im, const cv::KeyPoint &point, Color color, float
     return false;
 }
 
-bool is_black(const cv::Mat &im, const cv::KeyPoint &point, float tolerance = 40.0) {
+bool color::is_black(const cv::Mat &im, const cv::KeyPoint &point, float tolerance) {
     return same_color(im, point, Color::BLACK, tolerance);
 }
 
-bool is_red(const cv::Mat &im, const cv::KeyPoint &point, float tolerance = 40.0) {
+bool color::is_red(const cv::Mat &im, const cv::KeyPoint &point, float tolerance) {
     return same_color(im, point, Color::RED, tolerance);
 }
 
-} // namespace hw::util::color
-
-namespace geo {
-
-using UnitVector = cv::Point2f;
-
-geo::UnitVector connecting_vector(const cv::Point2f &a, const cv::Point2f &b) {
+geo::UnitVector geo::connecting_vector(const cv::Point2f &a, const cv::Point2f &b) {
     float x = b.x - a.x;
     float y = b.y - a.y;
     float length = std::sqrt(std::pow(x, 2) + std::pow(y, 2));
@@ -108,27 +61,18 @@ geo::UnitVector connecting_vector(const cv::Point2f &a, const cv::Point2f &b) {
 }
 
 // see https://stackoverflow.com/questions/25642532/opencv-pointx-y-represent-column-row-or-row-column
-void to_cartesian(cv::Point2f &point) {
+void geo::to_cartesian(cv::Point2f &point) {
     point.y *= -1;
 }
 
-} // namespace hw::util::geo
 
-namespace calc {
-
-void rotate(cv::Point2f &point, units::Degrees degrees) {
+void calc::rotate(cv::Point2f &point, units::Degrees degrees) {
     float angle = degrees.to_rad();
     auto x = point.x; // original x is saved for later calculation
     point.x = point.x * std::cos(angle) - point.y * std::sin(angle);
     point.y = x * std::sin(angle) + point.y * std::cos(angle);
 }
 
-float norm(const cv::Point2f &point) {
+float calc::norm(const cv::Point2f &point) {
     return std::sqrt(std::pow(point.x, 2) + std::pow(point.y, 2));
 }
-
-} // namespace hw::util::calc
-} // namespace hw::util
-} // namespace hw
-
-#endif //HWCPP_UTIL_HPP
