197570b990f8653295d7e2b38237f0336db43865
juliangaal <juliangaal@protonmail.com>
2019-05-27 22:56:42 +0200

    add various examples of image manipulation with numpy + opencv

--- scripts/encoder.py
+++ scripts/encoder.py
@@ -4,16 +4,88 @@ import random
 import numpy as np
 import cv2
 
-#let's create a 6 x 6 matrix with all pixels in black color
 size = 1000
 img = np.zeros((size,size,3),np.uint8)
 
-#let's use "for" cycle to change colorspace of pixel in a random way
 for x in range(size):
     for y in range(size):
         value = random.randint(0,1)
         if value == 1:
             img[x,y] = [255,255,255]
 
-#save our image as a "png" image
-cv2.imwrite("output.png",img)
+cv2.imwrite("output1.png",img)
+
+size = 1000
+img = np.zeros((size,size,3),np.uint8)
+
+for x in range(size):
+    for y in range(size):
+        value = random.randint(0,2)
+        if value == 1:
+            img[x,y] = [255,255,255]
+        if value == 2:
+            img[x,y] = [128,128,128]
+
+cv2.imwrite("output2.png",img)
+
+size = 1000
+img = np.zeros((size,size,3),np.uint8)
+
+for x in range(size):
+    for y in range(size):
+        if y % 5 == 0:
+            img[x,y] = [255,255,255]
+    
+cv2.imwrite("output3.png",img)
+
+size = 1000
+img = np.zeros((size,size,3),np.uint8)
+
+for x in range(size):
+    for y in range(size):
+        if x % 10 == 0:
+            img[x,y] = [255,255,255]
+
+cv2.imwrite("output4.png",img)
+
+size = 1000
+img = np.zeros((size,size,3),np.uint8)
+
+for x in range(size):
+    for y in range(size):
+        if y > size/2:
+            img[x,y] = [255,255,255]
+        
+cv2.imwrite("output5.png",img)
+
+size = 1000
+img = np.zeros((size,size,3),np.uint8)
+
+for x in range(size):
+    for y in range(size):
+        if y > size/2 and y < size/4:
+            img[x,y] = [255,255,255]
+
+cv2.imwrite("output6.png",img)
+
+size = 1000
+img = np.zeros((size,size,3),np.uint8)
+
+for x in range(size):
+    for y in range(size):
+        value = random.randint(0,1)
+        if value == 1:
+            img[x,y] = [255,255,255]
+
+cv2.imwrite("output6.png",img)
+
+size = 1000
+img = np.zeros((size,size,3),np.uint8)
+
+for x in range(size):
+    for y in range(size):
+        value = random.randint(0,1)
+        if value == 1:
+            img[x,y] = [255,255,255]
+
+cv2.imwrite("output7.png",img)
