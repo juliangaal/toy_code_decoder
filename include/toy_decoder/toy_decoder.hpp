019138982574024ff1025eb7c2699104201d7fea
Julian Gaal <gjulian@uos.de>
2019-05-28 11:20:39 +0200

    proper comments

--- include/toy_decoder/toy_decoder.hpp
+++ include/toy_decoder/toy_decoder.hpp
@@ -12,39 +12,86 @@
 
 namespace toy_decoder {
 
+/**
+ * Mark keypoints in image with Mark_KeyPoints::YES
+ */
 enum Mark_Keypoints {
     YES,
     NO,
 };
 
+/**
+ * Handles all operations necessary to decode Code (sample seen in README.md)
+ */
 class ToyDecoder {
 public:
+    /*
+     * Init with OpenCV image
+     */
     ToyDecoder(cv::Mat &img);
 
+    /*
+     * Init with OpenCV Image and custom Simpleblobdetector params
+     */
     ToyDecoder(cv::Mat &img, cv::SimpleBlobDetector::Params params);
 
+    /// default constructor deleted
     ToyDecoder() = delete;
 
+    /// default destructor
     ~ToyDecoder() = default;
 
+    /**
+     * Calculates keypoints
+     * @param mark: marks keypoint when chosen (optional)
+     */
     void calculate_keypoints(Mark_Keypoints mark);
 
+    /**
+     * Calculates orientation from keypoint
+     * @return orientation, success (true if successful)
+     */
     std::tuple<float, bool> calculate_orientation();
 
+    /**
+     * Rotate keypoints by specified amout
+     * @param degrees amount to rotate
+     */
     void rotate_keypoints(toy_decoder::util::units::Degrees degrees);
 
+    /**
+     * Rotate opencv image
+     * @param degrees amount to rotate
+     */
     void rotate_img(toy_decoder::util::units::Degrees degrees);
 
+    /**
+     * Decodes code
+     * @return cv::Point2i with coordinates and success or not
+     */
     std::tuple<cv::Point2i, bool> decode();
 
+    /**
+     * Saves image
+     * @param name to save with (optional)
+     */
     void save_img(std::string name = "output.jpg");
 
+    /**
+     * Opens opencv window with image in current state
+     * @param name of the window (optional)
+     */
     void open_img(std::string name = "img");
 private:
+    /// opencv image
     cv::Mat &_img;
+    /// simple blobdetector
     cv::SimpleBlobDetector::Params _params;
+    /// vector to save keypoints
     std::vector<cv::KeyPoint> _keypoints;
+    /// cv::Keypoint to save orientation point
     cv::KeyPoint _orientation_point;
+    /// avg_size used to calculate avg size of all detected rectangles
     float _avg_size;
 };
 
--- include/toy_decoder/util.hpp
+++ include/toy_decoder/util.hpp
@@ -19,33 +19,56 @@ constexpr T to(const S&& val) {
     return static_cast<T>(val);
 }
 
+/// float version of M_PI
 constexpr static float PIf = static_cast<float>(M_PI);
 
+/*
+ * decode translates vector of keypoints with differing sizes in binary notation to a number
+ * objects bigger than avg_size are treated as 1s
+ * objects smaller than avg_size are treated as 0s
+ */
 int decode(std::vector<cv::KeyPoint>::const_iterator begin,
            std::vector<cv::KeyPoint>::const_iterator end, float avg_size);
 
 namespace units {
 
+/**
+ * Helper class to avoid calculation errors between degrees and radians
+ * Used for more explicit function parameters, e.g. toy_decoder::rotate_keypoints
+ */
 class Degrees {
 public:
+    /// constructor
     inline explicit constexpr Degrees(float amount) : amount(amount) {}
 
+    /// default constructor deleted
     constexpr Degrees() = delete;
 
+    /// default destructor
     ~Degrees() = default;
 
+    /**
+     * converts degrees to rad
+     * @return value of amount as rad
+     */
     inline constexpr float to_rad() {
         return this->amount * PIf / 180.0f;
     }
 
+    /**
+     * returns degrees
+     * @return amount, unchanged
+     */
     inline constexpr float to_deg() {
         return this->amount;
     }
 
 private:
+    /// saves amount of Degrees
     float amount;
 };
 
+/// User defined literal, syntactic sugar
 constexpr Degrees operator "" _deg(long double deg)
 {
     return Degrees{static_cast<float>(deg)};
@@ -55,19 +78,40 @@ constexpr Degrees operator "" _deg(long double deg)
 
 namespace geo {
 
+/// explicit UnitVector type for explicitness
 using UnitVector = cv::Point2f;
 
+/**
+ * Calculates the connecting vector between to points in space
+ * @param a Point 1
+ * @param b Point 2
+ * @return UnitVector of distance and orientation between a and b
+ */
 geo::UnitVector connecting_vector(const cv::Point2f &a, const cv::Point2f &b);
 
-// see https://stackoverflow.com/questions/25642532/opencv-pointx-y-represent-column-row-or-row-column
+/**
+ * Converts opencv coordinates in something usable, see
+ * see https://stackoverflow.com/questions/25642532/opencv-pointx-y-represent-column-row-or-row-column
+ * @param point
+ */
 void to_cartesian(cv::Point2f &point);
 
 } // namespace toy_decoder::util::geo
 
 namespace calc {
 
+/**
+ * Rotate point in 2d space aka rotation matrix
+ * @param point to rotate
+ * @param degrees amount of degrees to rotate
+ */
 void rotate(cv::Point2f &point, units::Degrees degrees);
 
+/**
+ * Calculates vector length
+ * @param point to calculate length from
+ * @return vector length
+ */
 float norm(const cv::Point2f &point);
 
 } // namespace toy_decoder::util::calc
